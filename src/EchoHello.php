d3a5fabb34bcb55b4487b0efbe2e69179e003ce3
小伟 <srj520w@gmail.com>
2018-07-13 16:05:55 +0800

    第一次传

---
+++ src/EchoHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Sublime Text 3.
+ * Filename:  EchoHello.php
+ * User:      xuliwei
+ * Date:      2018/7/13
+ * Time:      15:32
+ */
+namespace Hello;
+
+class EchoHello
+{
+    public static function say()
+    {
+        echo 'hello';
+    }
+}
